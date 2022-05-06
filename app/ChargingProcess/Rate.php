29371923b09143df8c0ec4179b29faf93d0a53cf
Shahab Gheysari <ShahabGheysari@gmail.com>
2022-05-06 23:20:22 +0430

    added Rate value object to hold rate prices

---
+++ app/ChargingProcess/Rate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\ChargingProcess;
+
+class Rate
+{
+
+    private Money $energy;
+    private Money $time;
+    private Money $transaction;
+
+    public function __construct(Money $energy, Money $time, Money $transaction)
+    {
+        $this->energy = $energy;
+        $this->time = $time;
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * @return Money
+     */
+    public function getEnergy(): Money
+    {
+        return $this->energy;
+    }
+
+    /**
+     * @return Money
+     */
+    public function getTime(): Money
+    {
+        return $this->time;
+    }
+
+    /**
+     * @return Money
+     */
+    public function getTransaction(): Money
+    {
+        return $this->transaction;
+    }
+
+}
