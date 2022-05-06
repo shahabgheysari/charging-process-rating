5c50535b9d1b55f24e5da5be78cc5c7e0aa1848c
Shahab Gheysari <ShahabGheysari@gmail.com>
2022-05-06 23:49:24 +0430

    added CDR

---
+++ app/ChargingProcess/CDR/CDR.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\ChargingProcess\CDR;
+
+class CDR
+{
+
+    private Energy $energy;
+    private Time $time;
+    private Transaction $transaction;
+
+    public function __construct(Energy $energy, Time $time, Transaction $transaction)
+    {
+        $this->energy = $energy;
+        $this->time = $time;
+        $this->transaction = $transaction;
+    }
+
+    /**
+     * @return Energy
+     */
+    public function getEnergy(): Energy
+    {
+        return $this->energy;
+    }
+
+    /**
+     * @return Time
+     */
+    public function getTime(): Time
+    {
+        return $this->time;
+    }
+
+    /**
+     * @return Transaction
+     */
+    public function getTransaction(): Transaction
+    {
+        return $this->transaction;
+    }
+
+}
