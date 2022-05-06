68785a2e725e723f85719046bcfb941fae2337aa
Shahab Gheysari <ShahabGheysari@gmail.com>
2022-05-07 00:22:36 +0430

    refactoring of Money tests to be AAA-compliant

--- tests/Unit/ChargingProcess/MoneyTest.php
+++ tests/Unit/ChargingProcess/MoneyTest.php
@@ -10,54 +10,74 @@ class MoneyTest extends TestCase
 
     public function testIntAmountPassedValueWillBeReturnedAmountWithDefaultPrecision()
     {
-        $money = new Money(5000);
-        $this->assertEquals('5000.000', $money->getAmount());
+        $sut = new Money(5000);
+
+        $result = $sut->getAmount();
+
+        $this->assertEquals('5000.000', $result);
     }
 
     public function testFloatAmountPassedValueWillBeReturnedAmountWithDefaultPrecision()
     {
-        $money = new Money(5000.21);
-        $this->assertEquals('5000.210', $money->getAmount());
+        $sut = new Money(5000.21);
+
+        $result = $sut->getAmount();
+
+        $this->assertEquals('5000.210', $result);
     }
 
     public function testStringAmountPassedValueWillBeReturnedAmountWithDefaultPrecision()
     {
-        $money = new Money('85412');
-        $this->assertEquals('85412.000', $money->getAmount());
+        $sut = new Money('85412');
+
+        $result = $sut->getAmount();
+
+        $this->assertEquals('85412.000', $result);
     }
 
     public function testAddingTwoAmountsWillReturnCorrectSumWithDefaultPrecision()
     {
-        $money = new Money('5000.23');
-        $result = $money->add(new Money('2000'));
+        $sut = new Money('5000.23');
+
+        $result = $sut->add(new Money('2000'));
+
         $this->assertEquals('7000.230', $result->getAmount());
     }
 
     public function testAddingTwoAmountsWillReturnCorrectSumWithTwoPrecision()
     {
-        $money = new Money('5000.23', 2);
-        $result = $money->add(new Money('2000', 2));
+        $sut = new Money('5000.23', 2);
+
+        $result = $sut->add(new Money('2000', 2));
+
         $this->assertEquals('7000.23', $result->getAmount());
     }
 
     public function testSubtractingTwoAmountsWillReturnCorrectSumWithTwoPrecision()
     {
-        $money = new Money(5000.236, 2);
-        $result = $money->subtract(new Money(2000));
+        $sut = new Money(5000.236, 2);
+
+        $result = $sut->subtract(new Money(2000));
+
         $this->assertEquals('3000.23', $result->getAmount());
     }
 
     public function testMultiplyWithDefaultPrecision()
     {
-        $money = new Money(23);
-        $result = $money->multiply(3);
+        $sut = new Money(23);
+
+        $result = $sut->multiply(3);
+
         $this->assertEquals('69.000', $result->getAmount());
     }
 
 
     public function testMoneyZeroPrecision()
     {
-        $money = new Money(5000.7777, 0);
-        $this->assertEquals('5000', $money->getAmount());
+        $sut = new Money(5000.7777, 0);
+
+        $result = $sut->getAmount();
+
+        $this->assertEquals('5000', $result);
     }
 }
